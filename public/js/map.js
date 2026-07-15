162a35b41d293f448f3851bfe10a947a75df4cb1
Aditya Rajpoot <adityarajpoot1506@gmail.com>
2026-07-15 16:48:21 +0530

    Add Projects files

---
+++ public/js/map.js
@@ -0,0 +1,43 @@
+// mapboxgl.accessToken = mapToken;
+
+// const map = new mapboxgl.Map({
+//   container: "map",
+//   style: "mapbox://styles/mapbox/streets-v12",
+//   center: listing.geometry.coordinates, 
+//   zoom: 9,
+// });
+
+
+// const marker = new mapboxgl.Marker({ color: "red"})
+//   .setLngLat(listing.geometry.coordinates)
+//   .setPopup(new mapboxgl.Popup({offset: 25})
+//     .setHTML(`<h4>${listing.title}</h4><p>Exact Location will be provided after booking</p>`))
+//   .addTo(map);
+
+
+mapboxgl.accessToken = mapToken;
+
+if (listing.geometry && listing.geometry.coordinates && listing.geometry.coordinates.length === 2) {
+  const map = new mapboxgl.Map({
+    container: "map",
+    style: "mapbox://styles/mapbox/streets-v12",
+    center: listing.geometry.coordinates,
+    zoom: 12,
+  });
+
+  // Custom marker element
+  const el = document.createElement("div");
+  el.className = "custom-marker";
+  el.innerHTML = `<i class="fa-solid fa-house"></i>`;
+
+  const marker = new mapboxgl.Marker(el)
+    .setLngLat(listing.geometry.coordinates)
+    .setPopup(
+      new mapboxgl.Popup({ offset: 25 }).setHTML(
+        `<h4>${listing.title}</h4><p>Exact location provided after booking</p>`
+      )
+    )
+    .addTo(map);
+} else {
+  document.getElementById("map").innerHTML = "<p>Location not available for this listing.</p>";
+}
