70e1d04c0665eeb2b37d2ea015969207a06fbc24
Aditya Rajpoot <adityarajpoot1506@gmail.com>
2026-07-22 01:08:39 +0530

    Update listing.js

--- controllers/listing.js
+++ controllers/listing.js
@@ -12,24 +12,6 @@ module.exports.renderNewForm = (req, res) => {
   res.render("listings/new.ejs");
 };
 
-// module.exports.showListing = async (req, res) => {
-//   let { id } = req.params;
-//   const listing = await Listing.findById(id)
-//     .populate({
-//       path: "reviews",
-//       populate: {
-//         path: "author",
-//       },
-//     })
-//     .populate("owner");
-//   if (!listing) {
-//     req.flash("error", "Listing you requested for doesn't exist!");
-//     return res.redirect("/listings");
-//   }
-//   console.log(listing);
-//   res.render("listings/show.ejs", { listing });
-// };
-
 module.exports.showListing = async (req, res) => {
   let { id } = req.params;
   const listing = await Listing.findById(id)
