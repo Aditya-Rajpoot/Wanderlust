8332ac55281b1c9998664554bc41ed9bcaefc917
Aditya Rajpoot <adityarajpoot1506@gmail.com>
2026-07-23 13:02:02 +0530

    Update map.js

--- public/js/map.js
+++ public/js/map.js
@@ -1,12 +1,4 @@
 
-
-// const marker = new mapboxgl.Marker({ color: "red"})
-//   .setLngLat(listing.geometry.coordinates)
-//   .setPopup(new mapboxgl.Popup({offset: 25})
-//     .setHTML(`<h4>${listing.title}</h4><p>Exact Location will be provided after booking</p>`))
-//   .addTo(map);
-
-
 mapboxgl.accessToken = mapToken;
 
 if (listing.geometry && listing.geometry.coordinates && listing.geometry.coordinates.length === 2) {
